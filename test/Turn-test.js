37bf2d0a146308821d5577e5fb03392121913bd1
Madison Randle <madisonrandle25@gmail.com>
2020-03-18 17:16:10 -0600

    Write tests to see that user guess is returning the appropriate feedback

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -15,7 +15,7 @@ describe('Turn', () => {
 
   beforeEach(() => {
     card = new Card(data.prototypeData[0]);
-    turn = new Turn('object', card);
+    turn = new Turn('function', card);
   });
 
   it('should be a function', () => {
@@ -27,8 +27,8 @@ describe('Turn', () => {
   });
 
   it('should accept a user\'s guess', () => {
-    expect(turn.userGuess).to.be.a('string');
-    expect(turn.userGuess).to.equal('object');
+    // expect(turn.userGuess).to.be.a('string');
+    expect(turn.userGuess).to.equal('function');
   });
 
   it('should accept a card\'s content for each play', () => {
@@ -37,14 +37,36 @@ describe('Turn', () => {
   });
 
   it('should return a user\'s guess', () => {
-    expect(turn.returnGuess()).to.equal('object');
+    expect(turn.returnGuess()).to.be.a('string');
+    expect(turn.returnGuess()).to.equal('function');
   });
 
   it('should return a card\'s content for each play', () => {
+    expect(turn.returnCard()).to.be.an('object');
     expect(turn.returnCard()).to.equal(card);
   });
 
+  it('should evaluate a user\'s guess', () => {
+    expect(turn.evaluateGuess()).to.be.a('boolean');
+  });
 
+  it('should return true if a user guesses correctly', () => {
+    const turn = new Turn('object', card);
+    expect(turn.evaluateGuess()).to.equal(true);
+  });
+
+  it('should return false if a user guesses incorrectly', () => {
+    expect(turn.evaluateGuess()).to.equal(false);
+  });
+
+  it('should return \'correct!\' if a user guesses correctly', () => {
+    const turn = new Turn('object', card);
+    expect(turn.giveFeedback()).to.equal('correct!')
+  });
+
+  it('should return \'incorrect!\' if a user guesses incorrectly', () => {
+    expect(turn.giveFeedback()).to.equal('incorrect!')
+  });
 });
 
 //
