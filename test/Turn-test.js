e509bc69e4b6ed225c37c4f06d30f13fa1e125e1
Madison Randle <madisonrandle25@gmail.com>
2020-03-18 14:47:09 -0600

    Set up Turn class test suite

---
+++ test/Turn-test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+// Class default properties
+// Class methods
+// Anything that updates class properties
+
+describe('Turn', () => {
+  let turn;
+
+  beforeEach(() => {
+    turn = new Turn()
+  });
+
+  it('should be a function', )
+});
+
+//
