30582c4fd824c2121cb3e24bcd3ccd50473c8926
Madison Randle <madisonrandle25@gmail.com>
2020-03-18 14:46:38 -0600

    Write sad test if no id

--- test/Card-test.js
+++ test/Card-test.js
@@ -6,43 +6,38 @@ const Card = require('../src/Card');
 describe('Card', function() {
   let card;
 
-  beforeEach(function() {
+  beforeEach(() => {
     card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     cardSad = new Card(Date.now(), 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
   });
 
-// Class default properties
-// Class methods
-// Anything that updates class properties
-
-
-  it('should be a function', function() {
+  it('should be a function', () => {
     const card = new Card();
     expect(Card).to.be.a('function');
   });
 
-  it('should be an instance of Card', function() {
+  it('should be an instance of Card', () => {
     const card = new Card();
     expect(card).to.be.an.instanceof(Card);
   });
 
-  it('should assign an id if one is not given', function() {
+  it('should assign an id if one is not given', () => {
     expect(cardSad.id).to.be.a('number');
   });
 
-  it('should have an id', function() {
+  it('should have an id', () => {
     expect(card.id).to.equal(1);
   });
 
-  it('should store a question', function() {
+  it('should store a question', () => {
     expect(card.question).to.equal('What is Robbie\'s favorite animal');
   });
 
-  it('should store a list of possible answers', function() {
+  it('should store a list of possible answers', () => {
     expect(card.answers).to.deep.equal(['sea otter', 'pug', 'capybara']);
   });
 
-  it('should store the correct answer', function() {
+  it('should store the correct answer', () => {
     expect(card.correctAnswer).to.equal('sea otter');
   });
 });
