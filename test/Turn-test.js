bacc5625f8709cf5ffde6b8bb4ae64a5dbdd9b56
Madison Randle <madisonrandle25@gmail.com>
2020-03-18 15:55:52 -0600

    Require card object and write initial test of function and instantiation

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,20 +1,34 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card');
 const Turn = require('../src/Turn');
+const data = require('../src/data');
 
 // Class default properties
 // Class methods
 // Anything that updates class properties
 
 describe('Turn', () => {
+  let card;
   let turn;
 
   beforeEach(() => {
-    turn = new Turn()
+    card = new Card(data.prototypeData[0]);
+    turn = new Turn('object', card);
   });
 
-  it('should be a function', )
+  it('should be a function', () => {
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', () => {
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should be instantiated with a guess and card object', () => {
+    expect(turn.userGuess).to.equal('object', card);
+  })
 });
 
 //
