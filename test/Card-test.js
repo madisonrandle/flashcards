18f5fbd0e84250a9b70a13dfaa847913262191a0
Madison Randle <madisonrandle25@gmail.com>
2020-03-17 20:28:31 -0600

    Add sad test for id

--- test/Card-test.js
+++ test/Card-test.js
@@ -8,7 +8,13 @@ describe('Card', function() {
 
   beforeEach(function() {
     card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    cardSad = new Card(Date.now(), 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
   });
+
+// Class default properties
+// Class methods
+// Anything that updates class properties
+
 
   it('should be a function', function() {
     const card = new Card();
@@ -18,6 +24,14 @@ describe('Card', function() {
   it('should be an instance of Card', function() {
     const card = new Card();
     expect(card).to.be.an.instanceof(Card);
+  });
+
+  it('should assign an id if one is not given', function() {
+    expect(cardSad.id).to.be.a('number');
+  });
+
+  it('should have an id', function() {
+    expect(card.id).to.equal(1);
   });
 
   it('should store a question', function() {
