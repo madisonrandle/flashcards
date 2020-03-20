a448dce0ce95672a1bee8bc3ea19fa1a02a3d3f9
Madison Randle <madisonrandle25@gmail.com>
2020-03-20 02:25:54 -0600

    Write test for start game method

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,9 @@
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+
 
 class Game {
   constructor() {}
@@ -12,6 +15,14 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+
+  startGame() {
+    const deck = new Deck(prototypeQuestions);
+    const round = new Round(deck);
+    this.currentRound = round;
+    this.printMessage(deck);
+    this.printQuestion(round);
   }
 }
 
